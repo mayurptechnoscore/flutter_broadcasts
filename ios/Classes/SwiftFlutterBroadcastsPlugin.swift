c2f455133527af51516972d0e601989badb19b4b
mayurptechnoscore <165388107+mayurptechnoscore@users.noreply.github.com>
2025-06-03 13:13:01 +0530

    Update SwiftFlutterBroadcastsPlugin.swift

--- ios/Classes/SwiftFlutterBroadcastsPlugin.swift
+++ ios/Classes/SwiftFlutterBroadcastsPlugin.swift
@@ -3,7 +3,7 @@ import UIKit
 
 public class SwiftFlutterBroadcastsPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "flutter_broadcasts", binaryMessenger: registrar.messenger())
+    let channel = FlutterMethodChannel(name: "flutter_broadcasts_4m", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterBroadcastsPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
